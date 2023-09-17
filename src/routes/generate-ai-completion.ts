fb0ff96fc072c00244d258218ec12912a6c4449f
Anderson Menezes <Andersonfielads2@gmail.com>
2023-09-17 19:01:02 -0300

    feat:added generate AI completion

--- src/routes/generate-ai-completion.ts
+++ src/routes/generate-ai-completion.ts
@@ -1,6 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { z } from "zod"
-import {createReadStream} from 'node:fs'
+import {streamToResponse,OpenAIStream} from 'ai'
 import { prisma } from "../lib/prisma";
 import {openai} from "../lib/openai"
 
@@ -8,11 +8,11 @@ export async function generateAICompletionRoute(app:FastifyInstance) {
     app.post('/ai/complete',async (req , reply) => {
         const bodySchema = z.object({
             videoId: z.string().uuid(),
-            template: z.string(),
+            prompt: z.string(),
             temperature: z.number().min(0).default(0.5),
         })
  
-        const {videoId,template,temperature} = bodySchema.parse(req.body)
+        const {videoId,prompt,temperature} = bodySchema.parse(req.body)
 
         const video = await prisma.video.findFirstOrThrow({
             where:{
@@ -25,7 +25,7 @@ export async function generateAICompletionRoute(app:FastifyInstance) {
             return reply.status(400).send({error:'Vídeo transcription was not generated yet.'})
         }
 
-        const promptMessage = template.replace('{transcription}', video.transcription)
+        const promptMessage = prompt.replace('{transcription}', video.transcription)
 
         const response = await openai.chat.completions.create({
             model:'gpt-3.5-turbo-16k',
@@ -33,8 +33,15 @@ export async function generateAICompletionRoute(app:FastifyInstance) {
             messages:[
                 {role: 'user',content: promptMessage}
             ],
+            stream:true,
         })
 
-        return response
+        const stream = OpenAIStream(response)
+        streamToResponse(stream,reply.raw,{
+            headers:{
+                'Access-Control-Allow-Origin':'*',
+                'Acess-Control-Allow-Methods':'GET, POST, PUT, DELETE,OPTIONS',
+            }
+        })
     })
 }
